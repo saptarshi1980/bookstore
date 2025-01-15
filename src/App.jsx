47f503b04c9f01d56ba63398556e0107cb24cee5
Saptarshi Sanyal <saptarshi.edp@gmail.com>
2025-01-15 11:23:51 +0530

    Course page added, Route done, but design not done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import Home from './Home/Home';
+import Course from './components/Course';
+import {Route,Routes} from "react-router-dom";
+import Courses from './Courses/Courses';
+
 
 function App() {
   return (
     <>
-    <Home />
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/course" element={<Courses />} />
+   </Routes>
     
     </>
   )
